4fc914809cc0552c7ec8a5e136fbcd3291c19501
Nancylovesdata <nancyeofreeman@gmail.com>
2024-09-09 10:40:54 +0000

    let,const,var

--- 01-Fundamentals-Part-1/practice/script.js
+++ 01-Fundamentals-Part-1/practice/script.js
@@ -1,25 +1,69 @@
 
 // Values and Variables
-console.log("Jonas");
-console.log(23);
+// console.log("Jonas");
+// console.log(23);
 
-let firstName = "Matilda";
+// let firstName = "Matilda";
 
-console.log(firstName);
-console.log(firstName);
-console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
 
 // Variable name conventions
-let jonas_matilda = "JM";
-let $function = 27;
+// let jonas_matilda = "JM";
+// let $function = 27;
 
-let person = "jonas";
-let PI = 3.1415;
+// let person = "jonas";
+// let PI = 3.1415;
 
-let myFirstJob = "Coder";
-let myCurrentJob = "Teacher";
+// let myFirstJob = "Coder";
+// let myCurrentJob = "Teacher";
 
-let job1 = "programmer";
-let job2 = "teacher";
+// let job1 = "programmer";
+// let job2 = "teacher";
 
-console.log(myFirstJob);
+// console.log(myFirstJob);
+
+
+//Data types
+// let javascriptIsFun = true
+// console.log(javascriptIsFun)
+
+
+// console.log(typeof javascriptIsFun)
+// console.log(typeof "Jonas")
+// console.log(typeof 23)
+
+
+// javascriptIsFun = 'YES';
+// console.log(typeof javascriptIsFun)
+
+// let year;
+// console.log(year)
+// console.log(typeof year)
+
+// year = 1998
+// console.log(typeof year)
+
+
+// let kojo = null;
+// console.log(typeof kojo)
+
+
+////////////////////////////////////
+// let, const and var
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+// const job;
+
+var job = 'programmer';
+job = 'teacher'
+
+lastName = 'Schmedtmann';
+console.log(lastName);
+
+
+
