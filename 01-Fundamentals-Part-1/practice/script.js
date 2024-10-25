2f20daa337c542bf7d010551ea91c9e57dd133eb
Nancylovesdata <nancyeofreeman@gmail.com>
2024-10-25 12:03:10 +0000

    truthy & falsy values

--- 01-Fundamentals-Part-1/practice/script.js
+++ 01-Fundamentals-Part-1/practice/script.js
@@ -240,3 +240,30 @@ let n = '1' + 1;
 
 n=n-1
 console.log(n)
+
+
+// 5 falsy values: 0, '', undefined, null, Nan
+// console.log (Boolean(0));
+// console.log (Boolean(undefined));
+// console.log (Boolean('Nancy'));
+// console.log (Boolean({}));
+// console.log (Boolean(''));
+
+
+
+const money = 0;
+// In the if statement, where money = 0, javascript convert the money = 0 into a boolean, which is false. So when the code is running and reaches the first block of code, it skips "Don't spend it all" since money is false, and runs the next block of code.
+if (money){ 
+    console.log("Don't spend it all");
+}else{
+    console.log('You should get a job!');
+}
+
+
+
+let height;
+if(height){
+    console.log('Yay! height is defined')
+}else{
+    console.log ('Height is undefined')
+}
