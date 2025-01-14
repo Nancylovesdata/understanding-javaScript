2e18b5908f218b6bbe8a6b41aae10d6b51e01156
Nancylovesdata <nancyeofreeman@gmail.com>
2025-01-14 13:11:32 +0000

    Thev conditional ternary operator

--- 01-Fundamentals-Part-1/practice/script.js
+++ 01-Fundamentals-Part-1/practice/script.js
@@ -385,4 +385,26 @@ if (23 > 10) {
   const str = " 23 is bigger";
 }
 
+// The conditional ternary operator
+// This operator helps you to write statement similar to if-else statement but all in one line.
+// const age = 23;
+// age >= 18
+//   ? console.log(`I like to drink wine`)
+//   : console.log("I like to drink water");
 
+// commonly used
+// small block of code
+const age = 23;
+const drink = age >= 18 ? "wine" : "water";
+console.log(drink);
+
+// large block of code
+let drink2;
+if (age >= 18) {
+  drink2 = "wine";
+} else {
+  drink2 = "water";
+}
+console.log(drink2);
+
+console.log(`I like to drink ${age >= 18 ? "wine" : "water"}`);
