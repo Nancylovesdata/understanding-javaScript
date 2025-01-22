4bc657963c7253709b1862cbfb889a0a5c63f7f3
Nancylovesdata <nancyeofreeman@gmail.com>
2025-01-22 23:49:24 +0000

    strict mode

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,16 @@
+"use strict";
+// use strict is a mode activated in javascript to help us avoid errors. It does this by forbiding us to do certain this, it alsoo creates visible errors in the developer code.
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriverLicense = true;
+if (hasDriversLicense) console.log("I can drive : D");
+
+// strict mode reserves that cannot be used as variables. Javascript peopkl have reservesd them for future use..
+
+// const interface = "audio";
+
+// const provate =534
+
+// comdt if
